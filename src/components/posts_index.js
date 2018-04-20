29cf533748b88e05d3b6215b55b43e30f50ba89b
pawel misiak <pawelmisiak@live.com>
2018-04-20 17:27:16 -0400

    finish work on linking the posts page to new post part. Now working on form for new post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
+// Link is for navigation instead of using anchor tags
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
   //this is a live cycle method
@@ -8,14 +11,40 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    // this is an object that does not contain built in map function
+    // this is why we need to use loadash function for objects :D
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    console.log(this.props.posts);
     return (
       <div>
-        Post Index
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+// return list of posts
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 
-export default connect(null, { fetchPosts })(PostsIndex);
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
